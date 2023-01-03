64d0fa5da704ab4b2071fcdc4be9932186f870cb
ridwanyah24 <ridwangames24@gmail.com>
2023-01-03 17:17:59 +0000

    n

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 def islower(c):
-    low = "abcdefghijklmnopqrstuvwxyz"
-    if c in low:
+    if ord(c) >= 97 and ord(c) <= 122:
         return True
     else:
         return False
