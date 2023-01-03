e855e36af48f22a113b8d5b085dccb04bcbf2640
ridwanyah24 <ridwangames24@gmail.com>
2023-01-03 23:15:38 +0000

    n

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -4,7 +4,7 @@ def fizzbuzz():
         if i % 3 == 0 and i % 5 == 0:
             print("FizzBuzz")
         elif i % 3 == 0:
-            print (fizz)
+            print ("Fizz")
         elif i % 5 == 0:
             print ("Buzz")
         else:
