ae49f82c29ebc6a5e514c9b619b0c7f690e8d394
ridwanyah24 <ridwangames24@gmail.com>
2023-01-05 17:42:16 +0000

    n

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -3,6 +3,5 @@ if __name__ == "__main__":
     import sys, hidden_4
     modules = dir(hidden_4)
     for i in modules:
-        if "__" in modules[i]:
-            continue
-        print(modules[i])
+        if i[:2] != "__":
+            print(i)
