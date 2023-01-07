270755f261ab567e5a82750b4efff96874644c1d
ridwanyah24 <ridwangames24@gmail.com>
2023-01-07 22:18:22 +0000

    n

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -2,6 +2,7 @@
 def multiple_returns(sentence):
     if sentence == "":
         sentence[:1] = None
+        return (0, None)
     new_tup = []
     new_tup.append(len(sentence))
     new_tup.append(sentence[0])
