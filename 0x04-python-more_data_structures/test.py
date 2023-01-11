818ce36c211a4c04da4101c572223121f31331cb
ridwanyah24 <ridwangames24@gmail.com>
2023-01-11 17:03:43 +0000

    n

--- 0x04-python-more_data_structures/test.py
+++ 0x04-python-more_data_structures/test.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-common_elements = __import__('3-common_elements').common_elements
+common_elements = __import__('4-only_diff_elements').only_diff_elements
 
 set_1 = { "Python", "C", "Javascript" }
 set_2 = { "Bash", "C", "Ruby", "Perl" }
