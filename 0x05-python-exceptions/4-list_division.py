3497386816dea44e35a00f4631650bb45a9ad063
ridwanyah24 <ridwangames24@gmail.com>
2023-01-25 14:12:37 +0000

    n

---
+++ 0x05-python-exceptions/4-list_division.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+def list_division(my_list_1, my_list_2, list_length):
+
+    new = []
+
+    for i in range(list_length):
+        try:
+            answer = my_list_1[i] / my_list_2[i]
+        except IndexError:
+            print("out of range")
+            answer = 0
+        except TypeError:
+            print("wrong type")
+            answer = 0
+        except ZeroDivisionError:
+            print("division by 0")
+            answer = 0
+        finally:
+            new.append(answer)
+    return new
