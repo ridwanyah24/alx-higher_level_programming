0bbe81ef95cda248a63ffc0bea912b0065ab261d
ridwanyah24 <ridwangames24@gmail.com>
2023-01-25 10:00:46 +0000

    n

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -7,11 +7,12 @@ def safe_print_list(my_list=[], x=0):
     Returns:
         The number of elements printed.
     """
+    num = 0
     for i in range(x):
         try:
             print(my_list[i], end="")
-            i += 1
+            num += 1
         except Exception:
             break
     print("")
-    return (i)
+    return (num)
