19e74e6d1038a52d3360c24ca49418792cd3ec58
ridwanyah24 <ridwangames24@gmail.com>
2023-01-25 09:49:17 +0000

    n

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -2,9 +2,9 @@
 def safe_print_list(my_list=[], x=0):
     for i in range(x):
         try:
-            print(my_list[i], end="");
-            i +=1
-        except:
+            print(my_list[i], end="")
+            i += 1
+        except Exception:
             break
     print("")
     return (i)
