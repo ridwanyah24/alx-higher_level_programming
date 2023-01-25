01bc18857a254e023b8ce97ceddec70c4d1ce71e
ridwanyah24 <ridwangames24@gmail.com>
2023-01-25 15:15:56 +0000

    n

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
 import sys
+
+
 def safe_print_integer_err(value):
     try:
         print("{:d}".format(value))
         return True
     except Exception as error:
-        print("Execption: ", error)
+        print("Execption: {}".format(sys.exc_info()[1]), file=sys.stderr)
         return False
