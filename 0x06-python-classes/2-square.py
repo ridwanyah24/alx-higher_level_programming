56a7067af79e2cc3aed5274d125d2f91d3e4abcb
ridwanyah24 <ridwangames24@gmail.com>
2023-01-28 22:14:07 +0000

    n

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,6 +1,12 @@
 #!/usr/bin/python3
+"""
+this is the class for the stuff
+"""
+
+
 class Square:
     """this class belongs to a square"""
+
     def __init__(self, size=0):
         self.__size = size
 
