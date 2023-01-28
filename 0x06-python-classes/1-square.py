d12000e50c3dc3a00a43194172c0240f0729c38f
ridwanyah24 <ridwangames24@gmail.com>
2023-01-28 21:45:40 +0000

    n

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""This is the provisional Square class"""
+
+
+class Square():
+    def __init__(self, size):
+        self.size = size
+
+"""This class inherits from Square"""
+
+
+class NewSquare(Square):
+    pass
