faefccf025bd0236e8cd14345055ecb4db803adb
ridwanyah24 <ridwangames24@gmail.com>
2023-01-28 22:11:29 +0000

    n

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,2 +1,9 @@
 #!/usr/bin/python3
-class 
+class Square:
+    def __init__(self, size=0):
+        self.__size = size
+
+        if (type(self.__size) != int):
+            raise TypeError("size must be integer")
+        if (self.__size < 0):
+            raise ValueError("size must be >= 0")
