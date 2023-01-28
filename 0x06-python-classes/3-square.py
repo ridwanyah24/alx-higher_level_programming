6ba8dfc62c0b2aeb17833b6db826693ca2b02644
ridwanyah24 <ridwangames24@gmail.com>
2023-01-28 22:44:18 +0000

    n

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -6,9 +6,10 @@ this unit is the new square class
 
 class Square:
     """the new square class"""
+
     def __init__(self, size=0):
         self.__size = size
+
     def area(self):
         power = pow(self.__size, 2)
-
         return power
