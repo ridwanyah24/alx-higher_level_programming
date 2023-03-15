1d2b7cb5c4102ea801a96440cfb8a786d2443e49
ridwanyah24 <ridwangames24@gmail.com>
2023-03-15 08:21:37 +0000

    n

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 """ new stuff """
+
+
 def add_integer(a, b=0):
     """ Returns the addition of a and b
     Float arguments are converted to ints before addition is done
@@ -12,7 +14,7 @@ def add_integer(a, b=0):
 
     if ((not isinstance(a, int) and not isinstance(a, float))):
         raise TypeError("a must be an integer")
-    if ((not isinstance (b, int) and not isinstance (b, float))):
+    if ((not isinstance(b, int) and not isinstance(b, float))):
         raise TypeError("b must be an integer")
 
     return (int(a) + int(b))
