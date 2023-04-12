ecd55b97f4f3483fc7bc2d9dc4b2613af626fcb0
ridwanyah24 <ridwangames24@gmail.com>
2023-04-12 01:00:46 +0000

    unittest commit

---
+++ 0x07-python-test_driven_development/6-max_integer_test.py
@@ -0,0 +1,16 @@
+#!/bin/usr/python3
+
+
+def max_integer(list=[]):
+    """ returns the maximum of a list of integers
+        if the list is empty, the function returns None
+    """
+    if len(list) == 0:
+        return None
+    result = list[0]
+    i = 1
+    while i < len(list):
+        if list[i] > result:
+            result = list[i]
+        i += 1
+    return result
