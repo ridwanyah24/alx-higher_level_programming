dc2d3f9b4cd70d95076fea45a3122b0d56de930a
ridwanyah24 <ridwangames24@gmail.com>
2023-04-14 00:12:22 +0000

    n

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -2,5 +2,6 @@
 
 
 def lookup(obj):
-    """ this function lists the available attributes and objects of an object"""
+    """ this function lists the
+    available attributes and objects of an object"""
     return (dir(obj))
