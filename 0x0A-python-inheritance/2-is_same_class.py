fc30e40f6432a7d01ced6069ecf9e0c711c0916f
ridwanyah24 <ridwangames24@gmail.com>
2023-04-16 22:55:01 +0000

    n

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-""" this class returns true if the object is exactly an instance of the
-specified class otherwise false"""
 
 
 def is_same_class(obj, a_class):
+    """ this class returns true if the object is exactly an instance of the
+    specified class otherwise false"""
     if type(obj) == a_class:
         return True
     else:
--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-
+""" this stuff is boring"""
 
 def inherits_from(obj, a_class):
     """ this function does stuff that i want it to do!
