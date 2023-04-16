f5302408b2483cbbe7f4284bc32090416f53e6a1
ridwanyah24 <ridwangames24@gmail.com>
2023-04-16 22:16:24 +0000

    n

--- 0x0A-python-inheritance/6-base_geometry.py
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -1,13 +1,15 @@
 #!/usr/bin/python3
-
-
 """ this class does nothing"""
+
+
 class BaseGeometry():
     pass
 
 
 """ this class does nothing based on the previous class that does nothing as
 well"""
+
+
 class BaseGeometry(BaseGeometry):
     """ this function calculates the area of a shape"""
     def area(self):
