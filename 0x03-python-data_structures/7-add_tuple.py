9e32afff8e8bb7c5e7943d964ddda3b66794e1f2
ridwanyah24 <ridwangames24@gmail.com>
2023-08-29 20:58:04 +0000

    n

---
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+def add_tuple(tuple_a=(), tuple_b=()):
+    x, y = list(tuple_a), list(tuple_b)
+    if len(x) < 3:
+        for i in range(2 - (len(x) - 1)):
+            x.append(0)
+    if len(y) < 3:
+        for i in range(2 - (len(y) - 1)):
+            y.append(0)
+
+    new_tup = []
+    new_tup.append(x[0] + y[0])
+    new_tup.append(x[1] + y[1])
+    the_list = tuple(new_tup)
+
+    return the_list
