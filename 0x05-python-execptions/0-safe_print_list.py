d0ede98614a410c63e34be02876f73f8daff074b
ridwanyah24 <ridwangames24@gmail.com>
2023-10-23 16:54:47 +0000

    n

---
+++ 0x05-python-execptions/0-safe_print_list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+def safe_print_list(my_list=[], x=0):
+    ids = 0
+    try:
+        for i in range(0, x):
+            print("{}".format(my_list[i]), end="")
+            ids += 1
+    except IndexError:
+        pass
+    print("")
+    return ids
