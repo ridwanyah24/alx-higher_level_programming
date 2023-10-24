66d6e81e1e5cdba6a129aeaef94594c60978be7a
ridwanyah24 <ridwangames24@gmail.com>
2023-10-24 08:06:42 +0000

    n

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+class Square:
+    """This class defines a sqaure by 0-sqaure.py"""
+    def __init__(self, size):
+        """ This is the init method
+
+        Args: size which defines the size of the square
+        """
+        self.__size = size
+        pass
